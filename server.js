2df6243d660bdce51397718c456cf9426087011f
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-03-13 15:45:39 -0500

    update

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = 3003
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-app.listen(PORT, () => {
-  console.log('server is listening to ', PORT);
-})
