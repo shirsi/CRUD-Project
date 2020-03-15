a12bc609936b282387823b5427020ea749cd87ed
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-03-15 12:43:33 -0500

    css complete

--- frontend/recipes/src/App.js
+++ frontend/recipes/src/App.js
@@ -210,9 +210,9 @@ if(process.env.NODE_ENV === 'development'){
   render(){
   return (
 
-    <div className="App">
+    <div className="App .container">
       <header> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link> </header>
-      <div className = '.container'>
+      <div className = ' main'>
       <h1>Recipes</h1>
       <div className='recipe-container'>
       <New baseURL={baseURL} handleAddRecipe={this.handleAddRecipe}/>
@@ -250,7 +250,9 @@ if(process.env.NODE_ENV === 'development'){
         : null
       }
       </div>
+      <footer> Recipes</footer>
     </div>
+
     </div>
   )
 
